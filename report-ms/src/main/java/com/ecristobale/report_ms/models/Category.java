3505b5e467c6172b816adb308d3ea14e42010f42
CEED\Ceed <eduardo.cristobalenriquez0388@gmail.com>
2025-03-08 00:16:32 +0100

    Created report ms and its model classes

---
+++ report-ms/src/main/java/com/ecristobale/report_ms/models/Category.java
@@ -0,0 +1,11 @@
+package com.ecristobale.report_ms.models;
+
+public enum Category {
+    SOCIAL_NETWORK,
+    SERVICES,
+    STREAMING,
+    CLOUD_COMPUTING,
+    DEVICES,
+    EDUCATION,
+    NONE
+}
