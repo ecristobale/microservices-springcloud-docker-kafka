4b927893ceb09412c2db215935c028b5579bc08d
CEED\Ceed <eduardo.cristobalenriquez0388@gmail.com>
2025-03-08 00:38:07 +0100

    Created Loan Balancer config and repository for companies

---
+++ report-ms/src/main/java/com/ecristobale/report_ms/repositories/CompaniesRepository.java
@@ -0,0 +1,18 @@
+package com.ecristobale.report_ms.repositories;
+
+import com.ecristobale.report_ms.beans.LoadBalancerConfiguration;
+import com.ecristobale.report_ms.models.Company;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
+
+@FeignClient(name = "companies-crud")
+@LoadBalancerClient(name = "companies-crud", configuration = LoadBalancerConfiguration.class)
+public interface CompaniesRepository {
+
+    @GetMapping("/companies-crud/company/{name}")
+    Optional<Company> getByName(@PathVariable String name);
+}
