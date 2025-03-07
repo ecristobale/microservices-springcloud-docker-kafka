b79614e74701a88ed21b87b5417cbb0d435b9651
CEED\Ceed <eduardo.cristobalenriquez0388@gmail.com>
2025-03-07 15:02:09 +0100

    Defined compnay service interface

---
+++ companies-crud/src/main/java/com/ecristobale/companies_crud/services/CompanyService.java
@@ -0,0 +1,11 @@
+package com.ecristobale.companies_crud.services;
+
+import com.ecristobale.companies_crud.entities.Company;
+
+public interface CompanyService {
+
+    Company create(Company company);
+    Company readByName(String name);
+    Company update(Company company, String name);
+    void delete(String name);
+}
---
+++ companies-crud/src/main/java/com/ecristobale/companies_crud/services/CompanyServiceImpl.java
@@ -0,0 +1,25 @@
+package com.ecristobale.companies_crud.services;
+
+import com.ecristobale.companies_crud.entities.Company;
+
+public class CompanyServiceImpl implements CompanyService {
+    @Override
+    public Company create(Company company) {
+        return null;
+    }
+
+    @Override
+    public Company readByName(String name) {
+        return null;
+    }
+
+    @Override
+    public Company update(Company company, String name) {
+        return null;
+    }
+
+    @Override
+    public void delete(String name) {
+
+    }
+}
