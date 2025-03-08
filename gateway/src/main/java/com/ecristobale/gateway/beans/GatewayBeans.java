bf9af6c001bfbd34d15e9f89116f176b9462b32d
CEED\Ceed <eduardo.cristobalenriquez0388@gmail.com>
2025-03-08 19:23:25 +0100

    Configured dynamic routes

--- gateway/src/main/java/com/ecristobale/gateway/beans/GatewayBeans.java
+++ gateway/src/main/java/com/ecristobale/gateway/beans/GatewayBeans.java
@@ -1,24 +1,48 @@
 package com.ecristobale.gateway.beans;
 
+import io.netty.resolver.DefaultAddressResolverGroup;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
 public class GatewayBeans { //GatewayConfig
 
+//    @Bean
+//    @Profile(value = "eureka-off")
+//    public RouteLocator routeLocatorEurekaOff(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route(route -> route
+//                    .path("/companies-crud/company/*")
+//                    .uri("http://localhost:8081")
+//                )
+//                .route(route -> route
+//                    .path("/report-ms/report/*")
+//                    .uri("http://localhost:7070")
+//                )
+//                .build();
+//    }
+
+//    @Profile(value = "eureka-on")
     @Bean
-    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+    public RouteLocator routeLocatorEurekaOn(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(route -> route
-                    .path("/companies-crud/company/*")
-                    .uri("http://localhost:8081")
+                        .path("/companies-crud/company/*")
+                        .uri("lb://companies-crud") // lb: load balancer + name registered in Eureka
                 )
                 .route(route -> route
-                    .path("/report-ms/report/*")
-                    .uri("http://localhost:7070")
+                        .path("/report-ms/report/*")
+                        .uri("lb://report-ms")
                 )
                 .build();
+    }
+
+    @Bean
+    public HttpClient httpClient() {
+        return HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
     }
 }
